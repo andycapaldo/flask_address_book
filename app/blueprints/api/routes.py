bdb61cbf4e34a43b2d46eabad81bc27a547f3e4f
Andrew Capaldo <andycapaldo2@gmail.com>
2023-10-29 16:41:41 -0600

    Added HTTP Token Authentication, made it required for adding new addresses to the book

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -2,7 +2,7 @@ from flask import request
 from . import api
 from app import db
 from app.models import AddressBook
-from .auth import basic_auth
+from .auth import basic_auth, token_auth
 
 
 # Endpoint to get token - requires username/password
@@ -30,8 +30,9 @@ def get_address(address_id):
     return address.to_dict()
 
 
-# Endpoint to create a new address
-@api.route('/addresses', methods=['POST'])
+# Endpoint to create a new address entry - requires token authentication
+@api.route('/contacts', methods=['POST'])
+@token_auth.login_required
 def create_address():
     # Check to see that the request body is JSON
     if not request.is_json:
@@ -53,8 +54,11 @@ def create_address():
     phone_number = data.get('phone_number')
     address = data.get('address')
 
+    # Get the user
+    current_user = token_auth.current_user()
+
     # Create a new address to add to the database of addresses in our book!
-    new_address = AddressBook(first_name=first_name, last_name=last_name, phone_number=phone_number, address=address)
+    new_address = AddressBook(first_name=first_name, last_name=last_name, phone_number=phone_number, address=address, user_id=current_user.id)
     db.session.add(new_address)
     db.session.commit()
     return new_address.to_dict(), 201
